46c707c8febca375871936e9f90f6549ebc8a3fb
Shaqiel Mohamedhoesein <s.f.mohamedhoesein@hotmail.com>
2026-08-14 01:03:05 +0200

    Initial board display

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void display_board(char board[]);
+
+int main(){
+    char board[] {'1','2','3','4','5','6','7','8','9'};
+
+
+    //initial board display
+    display_board(board);
+}
+
+void display_board(char board[]){
+    std::cout << " " << board[0] << " | " << board[1] << " | " << board[2] << "\n";
+    std::cout << "---+---+---" << "\n";
+    std::cout << " " << board[3] << " | " << board[4] << " | " << board[5] << "\n";
+    std::cout << "---+---+---" << "\n";
+    std::cout << " " << board[6] << " | " << board[7] << " | " << board[8] << "\n";
+}
